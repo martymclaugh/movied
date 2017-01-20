cbd8f056a5142e4b267af37838b2e8b1711b44c0
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 17:51:45 -0800

    add movie preview route

--- src/config/routes.js
+++ src/config/routes.js
@@ -7,12 +7,14 @@ var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
 var MoviePrompt = require('../containers/MoviePrompt');
+var MoviePreview = require('../containers/MoviePreview');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
       <Route path='/search' component={MoviePrompt}/>
+      <Route path='/movie/:movieID' component={MoviePreview}/>
     </Route>
   </Router>
 );
