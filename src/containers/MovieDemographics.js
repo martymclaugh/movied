98400536845d0566253196597ce08070d2da34df
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-20 15:20:20 -0800

    make ajax call to webscraping server and display reponse on the page

--- src/containers/MovieDemographics.js
+++ src/containers/MovieDemographics.js
@@ -1,26 +1,51 @@
 var React = require('react');
 var helpers = require('../utils/helpers');
+var axios = require('axios');
 
 var MovieDemographics = React.createClass({
   getInitialState(){
     return {
-      ratings: []
+      ratings: undefined,
+      loading: true
     }
   },
   componentWillMount(){
     var imdbID = this.props.routeParams.movieID
     console.log(imdbID)
-    helpers.imdbScrape(imdbID).then(function(data){
+    var url = 'https://informationextractor.herokuapp.com/scrape/' + imdbID
+    console.log(url)
+    axios.get(url).then(function(response){
+      console.log(response);
       this.setState({
-        ratings: data
+        ratings: response.data.ratings,
+        loading: false
       })
-      console.log(this.state)
+      console.log(this.state, 'CURRENT STATE')
     }.bind(this))
   },
   render(){
-    return (
-      <div>{this.state.ratings}</div>
-    )
+    if (!this.state.loading){
+      return (
+        <div>
+        <p>DONE LOADING</p>
+        <p>{JSON.stringify(this.state.ratings)}</p>
+        {this.state.ratings.map((rating) => {
+          return(
+            <div>
+          <h1>Who: {rating.who}</h1>
+          <h2>How Many: {rating.amount}</h2>
+          <h2>Average Rating: {rating.rating}</h2>
+          </div>
+        )
+        })
+      }
+      </div>
+      )
+    } else {
+      return(
+        <h1>STILL LOADING</h1>
+      )
+    }
   }
 })
 
