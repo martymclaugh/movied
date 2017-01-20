7fa7416f3ed00082c02c0f853c8ad11149a9ebeb
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 19:50:41 -0800

    add imdb ajax methods

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,32 +1,63 @@
 var axios = require('axios');
+var cheerio = require('cheerio');
 
-function omdbData(response){
-
+function parseRatingsResponse(data){
+  data = data.split('See user ratings report for:')[1].split('Related Links')[0];
+  var newData = [];
+  data.split('\n').forEach(function(line){
+    newData.push(line.replace(/\s+/g,' ').trim());
+  })
+  newData = newData.filter(function(n){ return n !== '' && n !== 'Votes' && n !== 'Average'});
+  output = [];
+  newData.forEach(function(line){
+    let rating, amount;
+    var arr = line.split(' ');
+    var len = arr.length;
+    rating = arr.pop();
+    amount = arr.pop();
+    who = arr.join(' ');
+    output.push({who, rating, amount});
+  });
+  console.log(output);
+  return output;
 }
 
 var helpers = {
   omdbIdPing: function(id){
     var url = "http://www.omdbapi.com/?i=" + id + "&plot=full&r=json"
-    console.log(url)
     return axios.get(url).then(response => {
-      console.log(response.data)
       return(response.data)
     })
   },
   omdbTitlePing: function (movieTitle, movieYear){
-    console.log(movieTitle);
-    console.log(movieYear);
     if(movieYear.length > 0){
       return axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y="+ movieYear + "&plot=full&r=json").then(response => {
-        console.log(response.data)
         return(response.data)
       })
     } else {
       return axios.get("http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=full&r=json").then(response => {
-        console.log(response.data)
         return(response.data)
       })
     }
+  },
+  imdbScrape: function(id){
+    var config = {
+      headers: {"Access-Control-Allow-Origin": "*"}
+    }
+    url = 'http://www.imdb.com/title/' + id + '/ratings';
+    console.log(url)
+    axios.get(url).then(function(response){
+      if(response.status >= 200 && response.status < 300){
+        var $ = cheerio.load(response.data);
+        var title, release, rating;
+        var json = {};
+        var ratings = $('#tn15content').text();
+        var title = $('.main').text();
+      }
+      json.title = title;
+      json.ratings = parseRatingsResponse(ratings);
+      return json;
+  })
   }
 }
 
