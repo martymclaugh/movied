20ff031a6a640b2fd7bdbf1ffd7d27db7dced49b
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 18:58:43 -0800

    add demographics container

---
+++ src/containers/MovieDemographics.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var MovieDemographics = React.createClass({
+  render(){
+    return (
+      <div>Demographics</div>
+    )
+  }
+})
+
+module.exports = MovieDemographics;
