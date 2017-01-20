6af6a876c367c9638d192cda36421f7470dc900e
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 16:52:43 -0800

    add title and link to movie prompt

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,20 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
   render(){
     return (
-      <div>hello from home</div>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>movieD</h1>
+        <p className="lead">Get the rating demographics from iMDB</p>
+        <Link to="/search">
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button className="btn btn-block btn-success" type="submit">Continue</button>
+          </div>
+        </Link>
+      </div>
     )
   }
 });
