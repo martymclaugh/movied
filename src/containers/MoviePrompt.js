ad5b972ae77adc01cb655f2d466b4d3fded67b7b
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 16:53:13 -0800

    add initial state, update inputs to change state

--- src/containers/MoviePrompt.js
+++ src/containers/MoviePrompt.js
@@ -1,17 +1,40 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg
 
 var MoviePrompt = React.createClass({
+  getInitialState(){
+    return {
+      movieTitle: '',
+      movieYear: ''
+    }
+  },
+  function onUpdateMovieTitle(e){
+
+  },
+  function onUpdateMovieYear(e){
+
+  },
   render(){
     return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         <h1>Enter Desired Movie</h1>
         <div className="col-sm-12">
           <form >
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="Movie Title"/>
-              <input type="text" className="form-control" placeholder="(Optional) Movie Year"/>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Movie Title"
+                value={this.state.movieTitle}
+                onChange={this.onUpdateMovieTitle}/>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="(Optional) Movie Year"
+                value={this.state.movieYear}
+                onChange={this.onUpdateMovieYear}/>
             </div>
-            <div className="form-group col-sm-four col-sm-offset-4">
+            <div className="form-group col-sm-4 col-sm-offset-4">
               <button className="btn btn-block btn-success" type="submit">Continue</button>
             </div>
           </form>
