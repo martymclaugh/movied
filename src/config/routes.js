4011dd4e98f83b410a6b1c185dfb8eec94a4666a
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 16:38:44 -0800

    add movie prompt route

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,11 +6,13 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var MoviePrompt = require('../containers/MoviePrompt');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home}/>
+      <Route path='/search' component={MoviePrompt}/>
     </Route>
   </Router>
 );
