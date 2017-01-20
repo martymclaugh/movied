0a73ffd3694f92b3553a7698a19049eb1caea7ca
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-19 16:02:58 -0800

    add react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+var React = require('react');
+var ReactDOM = require('react-dom');
+// import App from './App';
+// import './index.css';
+
+var HelloWorld = React.createClass({
+  render(){
+    return (
+      <div>Hello {this.props.name}</div>
+    )
+  }
+})
 
 ReactDOM.render(
-  <App />,
+  // <App />,
+  <HelloWorld name="SHAZAM"/>,
   document.getElementById('app')
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: __dirname + '/src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: [
-    '.app/index.js'
+    './src/index.js'
   ],
   output: {
     path: __dirname + '/dist',
@@ -10,5 +17,6 @@ module.exports = {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 }
