225ab23357322f5a0f613efe87d0f80b35e5e9d5
Marty McLaughlin <martymclaugh@gmail.com>
2017-01-20 18:18:51 -0800

    add movie ratings graph component

--- src/components/MovieGraph.js
+++ src/components/MovieGraph.js
@@ -3,7 +3,11 @@ var React = require('react');
 var MovieGraph = React.createClass({
   render(){
     return(
-
+      <div>
+        {console.log(this.props.data)}
+      </div>
     )
   }
 })
+
+module.exports = MovieGraph;
